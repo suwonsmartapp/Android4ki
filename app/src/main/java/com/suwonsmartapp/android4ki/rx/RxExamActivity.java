341179ae45a6a6cea78ddecc12fd742003198512
Junsuk Oh <a811219@gmail.com>
2016-08-23 10:16:40 +0900

    RxJava, RxAndroid

--- app/src/main/java/com/suwonsmartapp/android4ki/rx/RxExamActivity.java
+++ app/src/main/java/com/suwonsmartapp/android4ki/rx/RxExamActivity.java
@@ -1,12 +1,16 @@
 package com.suwonsmartapp.android4ki.rx;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.suwonsmartapp.android4ki.R;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 import static com.suwonsmartapp.android4ki.R.id.text_view;
 
@@ -26,26 +30,37 @@ public class RxExamActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-        new MyTask().execute();
-    }
 
-    class MyTask extends AsyncTask<Void, Void, String> {
-
-
-        @Override
-        protected String doInBackground(Void... params) {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                try {
+                    Thread.sleep(5000);
+                    subscriber.onNext("변경 됨");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            return "변경 됨";
-        }
+        });
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            mTextView.setText(s);
-        }
+        observable.subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        mTextView.setText(s);
+                    }
+                });
     }
+
 }
